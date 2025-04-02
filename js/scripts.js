bf05112cf00225a2ae810d677ba3c3b8ae770d31
caamomilaa <c.velazquezdamico@gmail.com>
2025-04-02 09:59:08 +0200

    updt

--- js/scripts.js
+++ js/scripts.js
@@ -18,12 +18,12 @@
 const titleElement = document.getElementById('title');
 
 const changeTextOnHover = () => {
-  titleElement.textContent = 'Quita de encima!!!';
+	titleElement.textContent = 'Quita de encima!!!';
 };
 titleElement.addEventListener('mouseover', changeTextOnHover);
 
 const changeTextLeaving = () => {
-  titleElement.textContent = 'Soy un título';
+	titleElement.textContent = 'Soy un título';
 };
 titleElement.addEventListener('mouseout', changeTextLeaving);
 
@@ -32,9 +32,10 @@ titleElement.addEventListener('mouseout', changeTextLeaving);
 const buttonElement = document.getElementById('button');
 
 const getClickedTimes = () => {
-  //contador
+	//contador
 };
 buttonElement.addEventListener('click', getClickedTimes);
+
 // - Crea un p con el texto "esperando entrada de teclado..." al pulsar cualquier tecla deberá poner "has pulsado la tecla tal" y al soltarla el <p> volverá a tener el texto "esperando entrada de teclado...". Como reto extra puedes intentar añadir si se ha usado una combinación de teclas con alt, shift o control.
 
 // - Crea un array con 5 palabras, las que tú quieras. Añade un h2 a tu HTML. Añade dos botones con el texto previous y next respectívamente. Haz que los botones cambien el texto del h2 con las palabras del array, cuando llegues a la última volverás a la primera al pulsar el botón next y cuando estés en la primera podrás volver a la última haciendo click al botón previous.
